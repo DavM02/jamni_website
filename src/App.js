9d432ee74f6e60dc6d74b68b691b9b04328bd1a2
Dav <dmatosyan2002@gmail.com>
2024-11-14 17:04:40 +0400

    lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes, useLocation } from "react-router-dom";
+import React, { Suspense } from "react";
 import Footer from "./components/layout/Footer/Footer";
 import Header from "./components/layout/Header/Header";
 import SmoothScroll from "./components/scroll/SmoothScroll";
 import MainContextProvider from "./context/MainContext";
-import HomePage from "./pages/HomePage/HomePage";
-import CatalogPage from "./pages/CatalogPage/CatalogPage";
 import { AnimatePresence } from "framer-motion";
-import NewsPage from "./pages/NewsPage/NewsPage";
-import AboutPage from "./pages/AboutPage/AboutPage";
+ 
+
+const HomePage = React.lazy(() => import('./pages/HomePage/HomePage'))
+const AboutPage = React.lazy(() => import('./pages/AboutPage/AboutPage'))
+const NewsPage = React.lazy(() => import('./pages/NewsPage/NewsPage'))
 
 
 function App() {
@@ -22,13 +24,13 @@ function App() {
         <SmoothScroll>
           <main>
             <AnimatePresence mode="wait">
-              <Routes location={location} key={location.pathname} >
-                <Route path="/" element={<HomePage />}/>
-                <Route path="catalog" element={<CatalogPage/>} />
-                <Route path="news" element={<NewsPage  />} />
-                <Route path="about" element={<AboutPage />} />
-
-              </Routes>
+ 
+                <Routes location={location} key={location.pathname} >
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="news" element={<NewsPage />} />
+                  <Route path="about" element={<AboutPage />} />
+                </Routes>
+ 
             </AnimatePresence>
           </main>
           <Footer />
