fe122677953a847a17517cc298809f3702df8bc4
Dav <dmatosyan2002@gmail.com>
2024-11-16 00:05:26 +0400

    images optimized

--- src/components/sections/ProductsDisplay/ProductsDisplay.js
+++ src/components/sections/ProductsDisplay/ProductsDisplay.js
@@ -5,18 +5,26 @@ export default function ProductsDisplay() {
 
   const importAll = (r) => {
     let images = {};
-    r.keys().forEach((item) => {
-      if (item.includes('/')) {
+    let folderCount = {};
 
+    r.keys().forEach((item) => {
+      const folderName = item.substring(0, item.lastIndexOf('/'));
+
+      if (!folderCount[folderName]) {
+        folderCount[folderName] = 0;
+      }
+
+      if (folderCount[folderName] < 6) {
         images[item.replace('./', '')] = r(item);
+        folderCount[folderName]++;
       }
     });
+
     return images;
-  }
+  };
 
   const getImages = importAll(require.context('../../../assets', true, /\.(png|jpe?g|svg)$/));
 
-  console.log('mmmmm')
   return (
     <section id='products-display'>
       <div className='container'>
---
+++ src/data/decor.js
@@ -0,0 +1,237 @@
+import img1 from '../assets/decor/img_11.jpg';
+import img2 from '../assets/decor/img_12.jpg';
+import img3 from '../assets/decor/img_13.jpg';
+import img4 from '../assets/decor/img_14.jpg';
+import img5 from '../assets/decor/img_15.jpg';
+import img6 from '../assets/decor/img_16.jpg';
+import img7 from '../assets/decor/img_17.jpg';
+import img8 from '../assets/decor/img_18.jpg';
+import img9 from '../assets/decor/img_19.jpg';
+import img10 from '../assets/decor/img_20.jpg';
+import img11 from '../assets/decor/img_21.jpg';
+import img12 from '../assets/decor/img_22.jpg';
+import img13 from '../assets/decor/img_23.png';
+import img14 from '../assets/decor/img_24.jpg';
+
+const data = {
+    "decor": [
+        {
+            "id": 1,
+            "name": "foster",
+            "price": 5000,
+            "collection": "свечи",
+            "discount:": true,
+            "bestseller:": false,
+            "isNew": true,
+            "material": "мрамор",
+            "colors": ['palette-beige', 'palette-brown-light', 'palette-green', 'palette-gray'],
+            "img": img1
+        },
+        {
+            "id": 2,
+            "name": "Petrified",
+            "price": 7000,
+            "collection": "вазы и кашпо",
+            "discount:": false,
+            "bestseller:": true,
+            "isNew": false,
+            "material": "стекло",
+            "colors": ['white', 'palette-brown-light', 'palette-gray'],
+            "img": img2
+        },
+        {
+            "id": 3,
+            "name": "foster",
+            "price": 25000,
+            "collection": "вазы и кашпо",
+            "discount:": true,
+            "bestseller:": false,
+            "isNew": true,
+            "material": "керамика",
+            "colors": ['palette-brown', 'palette-gray-light'],
+            "img": img3
+        },
+        {
+            "id": 4,
+            "name": "ramani",
+            "price": 5000,
+            "collection": "картины",
+            "discount:": false,
+            "bestseller:": false,
+            "isNew": true,
+            "material": "ткань",
+            "colors": ['palette-beige', 'black', 'white', 'palette-brown'],
+            "img": img4
+        },
+        {
+            "id": 5,
+            "name": "Chest",
+            "price": 10000,
+            "collection": "другое",
+            "discount:": true,
+            "bestseller:": false,
+            "isNew": true,
+            "material": "кожа",
+            "colors": ['palette-green', 'white', 'palette-gray'],
+            "img": img5
+        },
+        {
+            "id": 6,
+            "name": "ramani",
+            "price": 5000,
+            "collection": "часы",
+            "discount:": false,
+            "bestseller:": true,
+            "isNew": false,
+            "material": "мрамор",
+            "colors": ['palette-gray', 'palette-brown'],
+            "img": img6
+        },
+        {
+            "id": 7,
+            "name": "ivory",
+            "price": 3000,
+            "collection": "другое",
+            "discount:": true,
+            "bestseller:": true,
+            "isNew": true,
+            "material": "керамика",
+            "colors": ['palette-beige', 'palette-gray-light', 'white'],
+            "img": img7
+        },
+        {
+            "id": 8,
+            "name": "Petrified",
+            "price": 3000,
+            "collection": "другое",
+            "discount:": false,
+            "bestseller:": false,
+            "isNew": false,
+            "material": "керамика",
+            "colors": ['palette-gray-light', 'white'],
+            "img": img8
+        },
+        {
+            "id": 9,
+            "name": "foster",
+            "price": 5000,
+            "collection": "другое",
+            "discount:": true,
+            "bestseller:": false,
+            "isNew": true,
+            "material": "мрамор",
+            "colors": ['palette-beige', 'palette-green', 'black'],
+            "img": img9
+        },
+        {
+            "id": 10,
+            "name": "foster",
+            "price": 5000,
+            "collection": "другое",
+            "discount:": false,
+            "bestseller:": true,
+            "isNew": false,
+            "material": "мрамор",
+            "colors": ['palette-brown-light', 'black'],
+            "img": img10
+        },
+        {
+            "id": 11,
+            "name": "foster",
+            "price": 6000,
+            "collection": "Картины",
+            "discount:": true,
+            "bestseller:": false,
+            "isNew": true,
+            "material": "стекло",
+            "colors": ['palette-gray', 'white'],
+            "img": img4
+        },
+        {
+            "id": 12,
+            "name": "ramani",
+            "price": 15000,
+            "collection": "вазы и кашпо",
+            "discount:": false,
+            "bestseller:": true,
+            "isNew": true,
+            "material": "керамика",
+            "colors": ['palette-beige', 'palette-brown', 'white', 'palette-gray'],
+            "img": img11
+        },
+        {
+            "id": 13,
+            "name": "foster",
+            "price": 8000,
+            "collection": "вазы и кашпо",
+            "discount:": false,
+            "bestseller:": false,
+            "isNew": true,
+            "material": "стекло",
+            "colors": ['palette-green', 'palette-gray-light'],
+            "img": img2
+        },
+        {
+            "id": 14,
+            "name": "foster",
+            "price": 5000,
+            "collection": "свечи",
+            "discount:": true,
+            "bestseller:": true,
+            "isNew": true,
+            "material": "мрамор",
+            "colors": ['white', 'palette-brown', 'black', 'palette-gray'],
+            "img": img12
+        },
+        {
+            "id": 15,
+            "name": "Petrified",
+            "price": 10000,
+            "collection": "другое",
+            "discount:": false,
+            "bestseller:": false,
+            "isNew": true,
+            "material": "мрамор",
+            "colors": ['palette-beige', 'black'],
+            "img": img13
+        },
+        {
+            "id": 16,
+            "name": "Ivory",
+            "price": 5000,
+            "collection": "другое",
+            "discount:": true,
+            "bestseller:": false,
+            "isNew": false,
+            "material": "мрамор",
+            "colors": ['palette-gray', 'palette-brown-light'],
+            "img": img9
+        },
+        {
+            "id": 17,
+            "name": "Chest",
+            "price": 20000,
+            "collection": "другое",
+            "discount:": false,
+            "bestseller:": true,
+            "isNew": false,
+            "material": "кожа",
+            "colors": ['palette-brown', 'palette-gray-light'],
+            "img": img5
+        },
+        {
+            "id": 18,
+            "name": "ramani",
+            "price": 5000,
+            "collection": "вазы и кашпо",
+            "discount:": true,
+            "bestseller:": false,
+            "isNew": true,
+            "material": "мрамор",
+            "colors": ['palette-beige', 'palette-gray', 'white'],
+            "img": img14
+        },
+    ]
+}
+
+export default data;
