7cd8b21a96a8c4b375375db22b77d18d8acaccaf
Dav <dmatosyan2002@gmail.com>
2024-11-17 14:38:45 +0400

    Updates

---
+++ static/js/189.740f600e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjamni=self.webpackChunkjamni||[]).push([[189],{5847:(n,e,s)=>{s.r(e),s.d(e,{default:()=>t});var a=s(3430),r=s(579);const t=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(a.A,{})})}}}]);
+//# sourceMappingURL=189.740f600e.chunk.js.map
