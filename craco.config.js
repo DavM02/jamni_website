cb65d4f2fdf84806a8887e3fb3ec9ce2adcc3bd2
Dav <dmatosyan2002@gmail.com>
2024-12-29 22:37:32 +0400

    upd

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,11 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
     webpack: {
         plugins: {
             add: [
+                new CleanWebpackPlugin(),
                 new MiniCssExtractPlugin({
                     filename: "static/css/[name].[contenthash].css",
                     chunkFilename: "static/css/[name].[contenthash].chunk.css",
@@ -13,6 +15,9 @@ module.exports = {
         configure: (webpackConfig) => {
             webpackConfig.optimization.splitChunks = {
                 cacheGroups: {
+                    default: {
+                        reuseExistingChunk: true,
+                    },
                     sharedStyles: {
                         name: "shared-styles",  
                         test: /src\/(components\/(ui|sections\/(moreproducts|about|projects|news\/newsItem)|cart\/.*|infiniteslider)\/.*\.css)$/i,  // Обновленное регулярное выражение
