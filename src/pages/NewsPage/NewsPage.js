60049fc2e80a5631b06ec9e78294a482cbee2fd1
Dav <dmatosyan2002@gmail.com>
2025-01-19 15:41:04 +0400

    upd

--- src/pages/NewsPage/NewsPage.js
+++ src/pages/NewsPage/NewsPage.js
@@ -1,15 +1,15 @@
 import React, { Suspense } from "react";
 import PageTransition from "../../components/PageTransition/PageTransition";
 import ChunkLoading from "../../components/ui/messages/ChunkLoading/ChunkLoading";
-
-const NewsSections = React.lazy(() => import("./NewsSections"));
+import NewsSections from "./NewsSections";
+// const NewsSections = React.lazy(() => import("./NewsSections"));
 
 function NewsPage() {
   return (
     <div id="news-page">
-      <Suspense fallback={<ChunkLoading />}>
+      {/* <Suspense fallback={<ChunkLoading />}> */}
         <NewsSections />
-      </Suspense>
+      {/* </Suspense> */}
     </div>
   );
 }
