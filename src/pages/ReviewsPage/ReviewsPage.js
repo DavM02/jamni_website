460ba0aa1e3eecc25adc412911cf5dfa3cc2d3fe
Dav <dmatosyan2002@gmail.com>
2025-01-19 15:18:39 +0400

    upd

--- src/pages/ReviewsPage/ReviewsPage.js
+++ src/pages/ReviewsPage/ReviewsPage.js
@@ -1,16 +1,16 @@
 import React, { Suspense } from "react";
 import PageTransition from "../../components/PageTransition/PageTransition";
 import ChunkLoading from "../../components/ui/messages/ChunkLoading/ChunkLoading";
-
-const ReviewsSections = React.lazy(() => import("./ReviewsSections"));
+import ReviewsSections from "./ReviewsSections";
+// const ReviewsSections = React.lazy(() => import("./ReviewsSections"));
 
 
 function ReviewsPage() {
   return (
     <div id="news-page">
-      <Suspense fallback={<ChunkLoading />}>
+      {/* <Suspense fallback={<ChunkLoading />}> */}
         <ReviewsSections />
-      </Suspense>
+      {/* </Suspense> */}
     </div>
   );
 }
