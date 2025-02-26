2029f2800f83a0929cf3dc2558b420ab3c7355e0
Dav <dmatosyan2002@gmail.com>
2025-02-26 20:01:49 +0400

    upd

--- src/components/Cursor/Cursor.js
+++ src/components/Cursor/Cursor.js
@@ -1,33 +1,32 @@
-import React, { useEffect, useRef } from 'react'
-import './cursor.css'
+import React, { useEffect, useRef } from 'react';
+import './cursor.css';
 import useMediaQ from '../../hooks/useMediaQ';
+
 export default function Cursor() {
-
-    const posRef = useRef(null)
-
+    const posRef = useRef(null);
     const query = useMediaQ('(max-width: 1024px)');
- 
+    const animationFrameId = useRef(null);  
 
     useEffect(() => {
-
-
         const updateCursor = (e) => {
-
             const { clientX, clientY } = e;
+
             if (posRef.current) {
-                posRef.current.style.transform = `translate(${posRef.current && clientX - posRef.current.offsetWidth / 2}px,
-            ${posRef.current && clientY - posRef.current.offsetHeight / 2}px)`;
+                cancelAnimationFrame(animationFrameId.current);  
+                animationFrameId.current = requestAnimationFrame(() => {
+                    posRef.current.style.transform = `translate(${clientX - posRef.current.offsetWidth / 2}px, 
+                        ${clientY - posRef.current.offsetHeight / 2}px)`;
+                });
             }
-        }
-        document.body.addEventListener('mousemove', updateCursor)
+        };
+
+        document.body.addEventListener('mousemove', updateCursor);
 
         return () => {
-            document.body.removeEventListener('mousemove', updateCursor)
+            document.body.removeEventListener('mousemove', updateCursor);
+            cancelAnimationFrame(animationFrameId.current);  
+        };
+    }, []);
 
-        }
-    }, [])
-    return (!query && <div className='custom-cursor' ref={posRef}>
-    </div>
-
-    )
+    return (!query && <div className='custom-cursor' ref={posRef}></div>);
 }
